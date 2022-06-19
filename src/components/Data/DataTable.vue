7550d056e1f7435bb269398da90236d93ef0969c
surmount96 <ayanwoye74@gmail.com>
2022-06-19 19:51:18 +0100

    manage when data could not be fetch state

--- src/components/Data/DataTable.vue
+++ src/components/Data/DataTable.vue
@@ -1,38 +1,40 @@
 <template>
-  <div
-    class="fixed bottom-0 bg-zinc-800 w-custom p-3 mx-3"
-    v-if="tables.length > 0"
-  >
-    <div class="flex justify-between text-md mb-4">
-      <p>Results</p>
-      <button class="flex items-center" @click="update">
-        <i class="ri-save-line mr-2 text-blue-600"></i>
-        Export data
-      </button>
+  <div class="fixed bottom-0 bg-zinc-800 w-custom p-3 mx-3">
+    <div class="" v-if="tables.length > 0">
+      <div class="flex justify-between text-md mb-4">
+        <p>Results</p>
+        <button class="flex items-center" @click="update">
+          <i class="ri-save-line mr-2 text-blue-600"></i>
+          Export data
+        </button>
+      </div>
+      <div class="h-64 overflow-y-scroll">
+        <div class="flex font-semibold text-sm border border-gray rounded">
+          <div
+            class="flex-1 px-3 py-2 border-r"
+            v-for="column in columns"
+            :key="column"
+          >
+            <p>{{ column }}</p>
+          </div>
+        </div>
+        <div
+          class="flex text-sm border border-gray rounded mb-4"
+          v-for="table in tables"
+          :key="table.id"
+        >
+          <div
+            class="flex-1 px-3 py-2 border-r"
+            v-for="(row, key) in table"
+            :key="row.id"
+          >
+            <p>{{ table[key] }}</p>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="h-64 overflow-y-scroll">
-      <div class="flex font-semibold text-sm border border-gray rounded">
-        <div
-          class="flex-1 px-3 py-2 border-r"
-          v-for="column in columns"
-          :key="column"
-        >
-          <p>{{ column }}</p>
-        </div>
-      </div>
-      <div
-        class="flex text-sm border border-gray rounded mb-4"
-        v-for="table in tables"
-        :key="table.id"
-      >
-        <div
-          class="flex-1 px-3 py-2 border-r"
-          v-for="(row, key) in table"
-          :key="row.id"
-        >
-          <p>{{ table[key] }}</p>
-        </div>
-      </div>
+    <div v-else>
+      <p>No Data found.</p>
     </div>
   </div>
 </template>
